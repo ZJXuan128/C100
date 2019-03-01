60c32bb37a1b3591ad80d3206dd9c20289c1c91b
ZSDDZA <lx19991208@gmail.com>
2019-03-01 19:19:53 +0800

    c100

--- c100/46.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main (void)
-{
-	int n=100;
-	printf("%d",n*(n+1)/2);
-}
--- c100/94.cpp
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(void)
-{
-	int m=1,i; 
-	for(i=0;i<20;i++)
-    	m*=2;
-	printf("%d",m);
-}
